ef7073c5723b30c61db219dcbab3bdcc971b9fa4
fidele22 <fidelehirwa2@gmail.com>
2022-10-19 22:46:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function displaynone(){
+  document.getElementById('display').style.display="none";
+}
+function hidde(){
+    document.getElementById('visibi').style.visibility="hidden";
+}
+function resetall(){
+    document.getElementById('display').style.display="block";
+    document.getElementById('visibi').style.visibility="visible";
+}
